5fc3400bc47039f3816b47ef00ad1a5a68d0af48
robi-rbz <robiaziz0110@bsi.ac.id>
2021-10-12 15:44:05 +0700

    form validasi

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pus taka-booking-3a/';
+$config['base_url'] = 'http://localhost/pustaka-booking-3a/';
 
 /*
 |--------------------------------------------------------------------------
